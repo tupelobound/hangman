054f2d752b0dcac380108401a8d857dc28c4b3c9
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-06-11 10:45:04 +0800

    Adds code for Task 2 and fixes word list strings

--- milestone_2.py
+++ milestone_2.py
@@ -1,5 +1,16 @@
+import random
+
 # create a list of five favourite fruits
-word_list = [cherry, blueberry, strawberry, apple, watermelon]
+word_list = ["cherry", "blueberry", "strawberry", "apple", "watermelon"]
 
 # print word list to standard out
 print(word_list)
+
+# initialise varable for random.choice method
+choice = random.choice
+
+# assign random word from list to word variable
+word = choice(word_list)
+
+# print word to standard out
+print(word)
