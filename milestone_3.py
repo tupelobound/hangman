3506b170c96d7a5f4e4a1c32001087b32b5bf599
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-06-12 16:06:47 +0800

    Adds continuous loop for user guess input

--- milestone_3.py
+++ milestone_3.py
@@ -1,1 +1,10 @@
-
+# Continuous loop to ask for user input until correct format of input is received
+while True:
+    # ask user to input a letter and save it to 'guess' variable
+    guess = input("Please enter a letter: ")
+    
+    # conditional to check that guess is an alphabetic character and only a single letter
+    if guess.isalpha() == True and len(guess) == 1:
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
