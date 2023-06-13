8cc3d9881e072638fc9dc46be4729dba44dff77b
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-06-13 16:32:45 +0800

    Moves check guessing and user input into functions

--- milestone_3.py
+++ milestone_3.py
@@ -1,16 +1,25 @@
-# Continuous loop to ask for user input until correct format of input is received
-while True:
-    # ask user to input a letter and save it to 'guess' variable
-    guess = input("Please enter a letter: ")
+# define function that takes a character and checks if it is in a word
+def check_guess(guess):
+    guess = guess.lower()
     
-    # conditional to check that guess is an alphabetic character and only a single letter
-    if guess.isalpha() == True and len(guess) == 1:
-        break
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-if guess.lower() in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")  
+# define function that asks for user input and then runs the check_guess function
+def ask_for_input():
+    while True:
+        # ask user to input a letter and save it to 'guess' variable
+        guess = input("Please enter a letter: ")
+        
+        # conditional to check that guess is an alphabetic character and only a single letter
+        if guess.isalpha() == True and len(guess) == 1:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
     
+    # call check_guess function and pass in the user input string
+    check_guess(guess)
+
+ask_for_input()
