e1f6dae5378c04c913ab2cfab68384389cb3d71c
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-06-13 18:13:52 +0800

    Fixes Hangman word_guessed attribute assignment

--- milestone_4.py
+++ milestone_4.py
@@ -3,7 +3,7 @@ import random
 class Hangman():
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice(word_list)
-        self.word_guessed = ["" for _ in self.word]
+        self.word_guessed = ["_" for _ in self.word]
         self.num_letters = 0
         self.num_lives = num_lives
         self.word_list = word_list
