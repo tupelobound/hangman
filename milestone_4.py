6198055509260caca1d56960bab736f841bc2051
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-06-13 17:54:20 +0800

    Adds Hangman class

--- milestone_4.py
+++ milestone_4.py
@@ -1,1 +1,10 @@
+import random
 
+class Hangman():
+    def __init__(self, word_list, num_lives=5):
+        self.word = random.choice(word_list)
+        self.word_guessed = []
+        self.num_letters = 0
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
