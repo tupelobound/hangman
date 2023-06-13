2a928a657d9762e3997577e4bb820ef0f1b34f2e
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-06-13 16:13:23 +0800

    Adds conditional to check if user guess is in word

--- milestone_3.py
+++ milestone_3.py
@@ -8,3 +8,9 @@ while True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess.lower() in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")  
+    
