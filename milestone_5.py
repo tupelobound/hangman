93f6cb2b039875f6cd94c155ad0d21eaa08d133e
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-06-15 20:51:29 +0800

    Adds logic to play_game function in milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -45,6 +45,17 @@ class Hangman():
                 break
 
 def play_game(word_list):
-    pass
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+    
+    while True:
+        if game.num_lives == 0:
+            print("You lost!")
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        else:
+            print("Congratulations! You won the game!")
+            break
 
+play_game(["strawberry"])
 
